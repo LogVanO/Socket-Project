b446484ac3765a1d1a14c35c10dd214707d562d3
bbnutt07 <bbnutt07@gmail.com>
2021-12-03 21:07:34 -0400

    sent messages are returned by server

--- app.js
+++ app.js
@@ -9,20 +9,20 @@ app.get("/", function(req, res) {
 //Whenever someone connects this gets executed
 io.on('connection', function(socket){
     console.log('A user connected');
-    
-    setTimeout(function(){
-        socket.send("Sent message")
-    }, 3000);
 
-    setTimeout(function(){
-        socket.send("Sent message 2")
-    }, 6000);
+    socket.send("Connected to chat server\r\n");
+
+    // return message to client
+    socket.on('clientEvent', function(data){
+        console.log(data);
+        socket.send(data);
+    });
 
     //Whenever someone disconnects this piece of code executed
     socket.on('disconnect', function () {
        console.log('A user disconnected');
     });
- });
+});
 
 http.listen(3000, function(){
     console.log('listening on *:3000');
